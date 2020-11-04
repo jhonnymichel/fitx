0ac3b02289c9c7a99c53a9f2fc98e73d2a39238e
Jhonny Michel <jhonnymichel@outlook.com>
2020-11-03 22:20:48 -0300

    adjusting padding

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,6 +1,6 @@
 function Card({ children }: { children: React.ReactNode }): JSX.Element {
   return (
-    <div className="w-full p-4 overflow-auto bg-white rounded-md shadow-lg md:p-6">{children}</div>
+    <div className="w-full p-2 overflow-auto bg-white rounded-md shadow-lg md:p-6">{children}</div>
   )
 }
 
