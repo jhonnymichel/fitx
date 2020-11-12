e42a48dbf89555d26cf873894de5608cdb3b83f8
Jhonny Michel <jhonnymichel@outlook.com>
2020-11-11 22:15:50 -0300

    improving icon component

--- app/auth/mutations/login.ts
+++ app/auth/mutations/login.ts
@@ -1,6 +1,6 @@
-import { Ctx } from "blitz"
-import { authenticateUser } from "app/auth/auth-utils"
-import { LoginInput, LoginInputType } from "../validations"
+import { Ctx } from 'blitz'
+import { authenticateUser } from 'app/auth/auth-utils'
+import { LoginInput, LoginInputType } from '../validations'
 
 export default async function login(input: LoginInputType, { session }: Ctx) {
   // This throws an error if input is invalid
--- app/components/icons/index.tsx
+++ app/components/icons/index.tsx
@@ -1,27 +1,47 @@
+import classNames from 'classnames'
 import FoodIcon from 'app/components/icons/colheita.svg'
 import CardioIcon from 'app/components/icons/tenis-de-corrida.svg'
 import StrengthIcon from 'app/components/icons/academia.svg'
 
+function IconBackground({
+  className,
+  children,
+}: {
+  className?: string
+  children: React.ReactNode
+}) {
+  return (
+    <div
+      className={classNames(
+        'flex w-16 h-16 p-2 bg-gray-200 rounded-full xl:p-3 xl:w-20 xl:h-20 place-items-center',
+        className
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+
 export function Food() {
   return (
-    <div className="flex w-16 h-16 p-2 text-green-900 bg-gray-200 rounded-full xl:p-3 xl:w-20 xl:h-20 place-items-center">
+    <IconBackground className="text-green-900">
       <FoodIcon className="w-full fill-current" />
-    </div>
+    </IconBackground>
   )
 }
 
 export function Cardio() {
   return (
-    <div className="flex w-16 h-16 p-2 text-blue-800 bg-gray-200 rounded-full xl:p-3 xl:w-20 xl:h-20 place-items-center">
+    <IconBackground className="text-blue-800">
       <CardioIcon className="w-full fill-current" />
-    </div>
+    </IconBackground>
   )
 }
 
 export function Strength() {
   return (
-    <div className="flex w-16 h-16 p-2 text-red-800 bg-gray-200 rounded-full xl:p-3 xl:w-20 xl:h-20 place-items-center">
+    <IconBackground className="text-red-800">
       <StrengthIcon className="w-full fill-current" />
-    </div>
+    </IconBackground>
   )
 }
