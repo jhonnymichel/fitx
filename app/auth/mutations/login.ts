ff17664f6c568795b41a9cdac735c0736905b41d
Jhonny Michel <jhonnymichel@outlook.com>
2021-03-05 02:25:39 -0300

    removing console log

--- app/auth/mutations/login.ts
+++ app/auth/mutations/login.ts
@@ -23,8 +23,6 @@ export default resolver.pipe(resolver.zod(Login), async ({ email, password }, ct
   // This throws an error if credentials are invalid
   const user = await authenticateUser(email, password)
 
-  console.log(ctx, ctx.session)
-
   await ctx.session.$create({ userId: user.id, role: user.role as Role })
 
   return user
