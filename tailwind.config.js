5f8d1dc49d84df944ae267efed913d4cf0aebe22
Jhonny Michel <jhonnymichel@outlook.com>
2023-02-20 00:54:26 -0500

    fixing tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   future: 'all',
-  purge: ['{app,pages}/**/*.{js,jsx,ts,tsx}'],
+  purge: ['{src,app,pages}/**/*.{js,jsx,ts,tsx}'],
   theme: {},
   variants: {},
   plugins: [],
