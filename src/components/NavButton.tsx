03dd124ab483766c0b1f14dea805781997cc9f14
Jhonny Michel <jhonnymichel@outlook.com>
2024-07-17 23:06:46 -0400

    Update NavButton.tsx

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -1,4 +1,6 @@
-function NavButton({ onClick, children, disabled }) {
+type NavButtonProps = { onClick: any; children: React.ReactNode; disabled?: boolean} 
+
+function NavButton({ onClick, children, disabled }: NavButtonProps) {
   return (
     <button disabled={disabled} className="px-4 text-white bg-teal-600 button" onClick={onClick}>
       {children}
